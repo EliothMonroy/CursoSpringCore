840ea1220bfae6c2404d49d3ce0b8a410365c7c8
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-09-22 14:56:19 -0500

    Clase 2 - practica 9, bean definition inheritance

--- 1-spring-core/src/test/java/org/certificatic/spring/core/practica9/test/beandefinheritance/bean/BeanDefinitionInheritanceTest.java
+++ 1-spring-core/src/test/java/org/certificatic/spring/core/practica9/test/beandefinheritance/bean/BeanDefinitionInheritanceTest.java
@@ -1,6 +1,11 @@
 package org.certificatic.spring.core.practica9.test.beandefinheritance.bean;
 
+import org.certificatic.spring.core.practica9.beandefinitioninheritance.bean.ConnectionDataBase;
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,6 +18,24 @@ public class BeanDefinitionInheritanceTest {
 		log.info("beanDefinitionInheritanceTest1 -------------------");
 
 		// Implementar
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"spring/practica9/bean-def-inheritance-application-context.xml");
+
+		ConnectionDataBase conexionProduccion = applicationContext.getBean("connectionProdBean",
+				ConnectionDataBase.class);
+
+		Assert.assertNotNull(conexionProduccion);
+
+		conexionProduccion.showInfo();
+
+		ConnectionDataBase conexionPruebas = applicationContext.getBean("connectionTestBean",
+				ConnectionDataBase.class);
+
+		Assert.assertNotNull(conexionPruebas);
+
+		conexionPruebas.showInfo();
+
+		((AbstractApplicationContext) applicationContext).close();
 	}
 
 	@Test
@@ -21,6 +44,24 @@ public class BeanDefinitionInheritanceTest {
 		log.info("beanDefinitionInheritanceTest2 -------------------");
 
 		// Implementar
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"spring/practica9/bean-def-inheritance-application-context2.xml");
+
+		ConnectionDataBase conexionProduccion = applicationContext.getBean("connectionProdBean",
+				ConnectionDataBase.class);
+
+		Assert.assertNotNull(conexionProduccion);
+
+		conexionProduccion.showInfo();
+
+		ConnectionDataBase conexionPruebas = applicationContext.getBean("connectionTestBean",
+				ConnectionDataBase.class);
+
+		Assert.assertNotNull(conexionPruebas);
+
+		conexionPruebas.showInfo();
+
+		((AbstractApplicationContext) applicationContext).close();
 	}
 
 }
