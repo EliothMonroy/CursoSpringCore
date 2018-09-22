8215a11eb4c87d0fe650abe28a0b2aca7722598e
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-09-22 14:02:15 -0500

    Clase 2 - practica 7, lazy init

--- 1-spring-core/src/test/java/org/certificatic/spring/core/practica7/test/lazyinit/LazyInitTest.java
+++ 1-spring-core/src/test/java/org/certificatic/spring/core/practica7/test/lazyinit/LazyInitTest.java
@@ -1,19 +1,31 @@
 package org.certificatic.spring.core.practica7.test.lazyinit;
 
+import org.certificatic.spring.core.practica7.lazyinit.bean.Car;
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LazyInitTest {
 
-	private static ApplicationContext applicationContext;
+	private static ConfigurableApplicationContext applicationContext;
 
 	@BeforeClass
 	public static void beforeClass() {
 		// Instanciar ApplicationContext
+		applicationContext = new ClassPathXmlApplicationContext(
+				"spring/practica7/lazy-init-application-context.xml");
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		// Apagar ApplicationContext
+		applicationContext.close();
 	}
 
 	@Test
@@ -22,6 +34,11 @@ public class LazyInitTest {
 		log.info("lazyInitTest -------------------");
 
 		// Implementar
+		Car car = applicationContext.getBean(Car.class);
+
+		Assert.assertNotNull(car);
+
+		log.info("car: {}", car);
 	}
 
 }
