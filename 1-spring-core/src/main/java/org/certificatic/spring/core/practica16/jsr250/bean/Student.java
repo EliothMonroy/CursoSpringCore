dc63771868201cdda8c79b4ee53c82200df3e42f
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-09-29 13:45:45 -0500

    Clase 3 - Practica 16 JSR 250

--- 1-spring-core/src/main/java/org/certificatic/spring/core/practica16/jsr250/bean/Student.java
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica16/jsr250/bean/Student.java
@@ -1,27 +1,38 @@
 package org.certificatic.spring.core.practica16.jsr250.bean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
 import lombok.Data;
 
 @Data
 public class Student {
 
 	// Inyeactar
+	@Resource
 	private String name;
 
 	// Inyeactar
+	@Resource
 	private String enrollment;
 
 	// Inyeactar
+	@Resource(name = "xyz")
 	private Subject subject;
 
 	// Inyeactar a mathematics subject
+	@Resource
+	private Subject mathematics;
 
 	// Inicializar
+	@PostConstruct
 	public void postConstructCallback() {
 		System.out.println("Initializing Student Bean");
 	}
 
 	// Destruir
+	@PreDestroy
 	public void preDestroyCallback() {
 		System.out.println("Destroying Student Bean");
 	}
--- 1-spring-core/src/test/java/org/certificatic/spring/core/practica16/test/jsr250/Jsr250AnnotationTest.java
+++ 1-spring-core/src/test/java/org/certificatic/spring/core/practica16/test/jsr250/Jsr250AnnotationTest.java
@@ -1,7 +1,10 @@
 package org.certificatic.spring.core.practica16.test.jsr250;
 
+import org.certificatic.spring.core.practica16.jsr250.bean.Student;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,11 +12,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Jsr250AnnotationTest {
 
-	private static ClassPathXmlApplicationContext applicationContext;
+	private static ConfigurableApplicationContext ctx;
 
 	@BeforeClass
 	public static void beforeClass() {
-		applicationContext = new ClassPathXmlApplicationContext(
+		ctx = new ClassPathXmlApplicationContext(
 				"spring/practica16/annotations-jsr250-application-context.xml");
 	}
 
@@ -23,6 +26,16 @@ public class Jsr250AnnotationTest {
 		log.info("jsr250AnnotationTest -------------------");
 
 		// Implementar
+		Student student = ctx.getBean(Student.class);
+
+		Assert.assertNotNull(student);
+		Assert.assertNotNull(student.getName());
+		Assert.assertNotNull(student.getEnrollment());
+		Assert.assertNotNull(student.getSubject());
+
+		log.info("student: {}", student);
+
+		ctx.close();
 	}
 
 }
