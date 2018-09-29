0f624d969c2cab1d3696cf8f632add7384b1ddd2
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-09-29 13:12:07 -0500

    Ya mero p15

--- 1-spring-core/src/test/java/org/certificatic/spring/core/practica15/test/autowired/properties/AutowiredPropertiesAnnotationTest.java
+++ 1-spring-core/src/test/java/org/certificatic/spring/core/practica15/test/autowired/properties/AutowiredPropertiesAnnotationTest.java
@@ -1,6 +1,11 @@
 package org.certificatic.spring.core.practica15.test.autowired.properties;
 
+
+import org.certificatic.spring.core.practica15.autowired.properties.bean.Reporter;
 import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Assert;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,6 +20,15 @@ public class AutowiredPropertiesAnnotationTest {
 		String ctxFile = "spring/practica15/annotations-autowired-property-application-context.xml";
 
 		// Implementar
+		AbstractApplicationContext ctx=new ClassPathXmlApplicationContext(ctxFile);
+		Reporter reporter=ctx.getBean(Reporter.class);
+		Assert.notNull(reporter);
+		Assert.notNull(reporter.getName());
+		Assert.notNull(reporter.getAge());
+		Assert.notNull(reporter.getBooklet());
+		Assert.notNull(reporter.getPencil());
+		log.info("reporter: {}",reporter);
+		ctx.close();
 	}
 
 }
