1e6c6c4bda282f1ecde84eb48b5ce48d93b40b91
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-09-29 13:04:03 -0500

    Ya casi p12

--- 1-spring-core/src/test/java/org/certificatic/spring/core/practica15/test/autowired/methods/AutowiredMethodsAnnotationTest.java
+++ 1-spring-core/src/test/java/org/certificatic/spring/core/practica15/test/autowired/methods/AutowiredMethodsAnnotationTest.java
@@ -1,6 +1,11 @@
 package org.certificatic.spring.core.practica15.test.autowired.methods;
 
+
+import org.certificatic.spring.core.practica15.autowired.methods.bean.Student;
 import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Assert;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,7 +20,15 @@ public class AutowiredMethodsAnnotationTest {
 		String ctxFile = "spring/practica15/annotations-autowired-methods-application-context.xml";
 
 		// Implementar
-		
+		AbstractApplicationContext ctx=new ClassPathXmlApplicationContext(ctxFile);
+		Student student=ctx.getBean(Student.class);
+		Assert.notNull(student);
+		Assert.notNull(student.getName());
+		Assert.notNull(student.getAge());
+		Assert.notNull(student.getPropellingPencil());
+		Assert.notNull(student.getStudentBook());
+		log.info("student: {}",student);
+		ctx.close();
 	}
 
 }
