9f43dd85f582a391a6fa07d4bb8f552e11eaa89d
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-10-06 13:32:22 -0500

    Práctica 20 lista

--- 1-spring-core/src/main/java/org/certificatic/spring/core/practica20/resources/bean/FavouriteRockBands.java
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica20/resources/bean/FavouriteRockBands.java
@@ -1,19 +1,26 @@
 package org.certificatic.spring.core.practica20.resources.bean;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
 @Data
 // Definir bean componente
+@Component("favouriteRockBands")
 public class FavouriteRockBands {
 
 	// Inyectar valor ${favourite.rockband.first}
+	@Value("${favourite.rockband.first}")
 	private String firstRockBand;
 
 	// Inyectar valor ${favourite.rockband.second}
+	@Value("${favourite.rockband.second}")
 	private String secondRockBand;
 
 	// Inyectar valor "My favourites rockbands are: ${favourite.rockband.first}
 	// and ${favourite.rockband.second}"
+	@Value("My favourites rockbands are: ${favourite.rockband.first} and ${favourite.rockband.second}")
 	private String toString;
 
 	public String toString() {
