2a800e1ef6c45fe6816a2e929df4af0567d9b353
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-10-06 11:09:15 -0500

    Nada

--- 1-spring-core/src/main/java/org/certificatic/spring/core/practica19/javaconfig/RepositoryConfig.java
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica19/javaconfig/RepositoryConfig.java
@@ -1,9 +1,13 @@
 package org.certificatic.spring.core.practica19.javaconfig;
 
 import org.certificatic.spring.core.practica19.javaconfig.bean.DummyRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
-//Habilitar Clase de configuración
+//Habilitar Clase de configuraciï¿½n
+@Configuration
 //Agregar Import de clases de Recurso xml
+@ImportResource(locations=("classpath:/spring/practica19/datasource-application-context.xml"))
 public class RepositoryConfig {
 
 	// Define Bean
