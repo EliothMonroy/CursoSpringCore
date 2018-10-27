ce21ac6d89287bc924986042cb008722608dd95a
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-10-27 14:25:40 -0500

    Clase 7 - Practica 26 - Fix XML test

--- 3-spring-data-jdbc/src/test/java/org/certificatic/spring/tx/practica26/test/springtx/parte1/DeclarativeXMLTransactionManagerTest.java
+++ 3-spring-data-jdbc/src/test/java/org/certificatic/spring/tx/practica26/test/springtx/parte1/DeclarativeXMLTransactionManagerTest.java
@@ -2,6 +2,8 @@ package org.certificatic.spring.tx.practica26.test.springtx.parte1;
 
 import org.certificatic.spring.tx.pratica26.springtx.domain.BusinessObject;
 import org.certificatic.spring.tx.pratica26.springtx.service.api.ITransactionalService;
+import org.certificatic.spring.tx.util.Color;
+import org.certificatic.spring.tx.util.bean.api.IColorWriter;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,12 +16,14 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-		locations = "classpath:/spring/practica26/spring-tx-application-context.xml")
+@ContextConfiguration(locations = "classpath:/spring/practica26/spring-tx-application-context.xml")
 public class DeclarativeXMLTransactionManagerTest {
 
 	@Autowired
 	private ITransactionalService transactionalService;
+
+	@Autowired
+	private IColorWriter colorWriter;
 
 	@Before
 	public void setUp() {
@@ -35,45 +39,61 @@ public class DeclarativeXMLTransactionManagerTest {
 		try {
 			bo = transactionalService.getBusinessObject(5L);
 		} catch (Exception ex) {
-			log.info(
-					"transactionalService.getBusinessObject(5L) throws Exception {}",
-					ex.getMessage());
+
+			log.info("{}", colorWriter.getColoredMessage(Color.RED,
+					"transactionalService.getBusinessObject(5L) throws Exception " +
+							ex.getMessage()));
 		}
 		log.info("getBusinessObject(5L): {}", bo);
+	}
 
-		log.info("\n----------------------------------------------");
+	@Test
+	public void declarativeXMLTransactionManagerTest2() {
+		log.info("declarativeXMLTransactionManagerTest2 -------------------");
 
 		log.info("calling: transactionalService.insertBusinessObject(null)");
 		try {
 			transactionalService.insertBusinessObject(null);
 		} catch (Exception ex) {
-			log.info(
-					"transactionalService.insertBusinessObject(null) throws Exception {}",
-					ex.getMessage());
-		}
 
-		log.info("\n----------------------------------------------");
+			log.info("{}", colorWriter.getColoredMessage(Color.RED,
+					"transactionalService.insertBusinessObject(null) throws Exception " +
+							ex.getMessage()));
+		}
+	}
+
+	@Test
+	public void declarativeXMLTransactionManagerTest3() {
+		log.info("declarativeXMLTransactionManagerTest3 -------------------");
 
 		log.info("calling: transactionalService.updateBusinessObject(bo)");
+
+		BusinessObject bo = BusinessObject.builder().build();
+
 		try {
 			transactionalService.updateBusinessObject(bo);
 		} catch (Exception ex) {
-			log.info(
-					"transactionalService.updateBusinessObject(bo) throws Exception {}",
-					ex.getMessage());
-		}
 
-		log.info("\n----------------------------------------------");
+			log.info("{}", colorWriter.getColoredMessage(Color.RED,
+					"transactionalService.updateBusinessObject(bo) throws Exception " +
+							ex.getMessage()));
+		}
+	}
+
+	@Test
+	public void declarativeXMLTransactionManagerTest4() {
+		log.info("declarativeXMLTransactionManagerTest4 -------------------");
 
 		log.info("calling: transactionalService.deleteBusinessObject(5L)");
 		try {
 			transactionalService.deleteBusinessObject(5L);
 		} catch (Exception ex) {
-			log.info(
-					"transactionalService.deleteBusinessObject(5L) throws Exception {}",
-					ex.getMessage());
+
+			log.info("{}", colorWriter.getColoredMessage(Color.RED,
+					"transactionalService.deleteBusinessObject(5L) throws Exception " +
+							ex.getMessage()));
+
 		}
 
-		log.info("\n----------------------------------------------");
 	}
 }
