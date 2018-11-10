e67882f66ad84a9725264c8924359308d118098c
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-11-10 15:21:43 -0600

    práctica 28

--- 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica28/controller/IndexController.java
+++ 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica28/controller/IndexController.java
@@ -1,17 +1,25 @@
 package org.certificatic.spring.mvc.practica28.controller;
 
 import org.certificatic.spring.mvc.practica28.rootbeans.BeanComponent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 // Anotar controller
+@Controller
 class IndexController {
 
 	// Inyectar required false
+	@Autowired(required=false)
 	private BeanComponent beanComponent;
 
 	// anotar request mapping a "/" y a "" por GET
+	@RequestMapping(value= {"/",""}, method=RequestMethod.GET)
 	public String showIndexPage(Model model) {
 		// Agregar mensaje "message" al modelo
+		model.addAttribute("message","Elioth !");
 		return "index";
 	}
 
