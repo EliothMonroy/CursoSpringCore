574221824425a34a33194ee5a835d8a87898c8cd
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-11-17 09:00:32 -0600

    Clase 9 - Practica 28 - Spring hello world parte 2

--- 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica28/controller/IndexController.java
+++ 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica28/controller/IndexController.java
@@ -4,8 +4,10 @@ import org.certificatic.spring.mvc.practica28.rootbeans.BeanComponent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // Anotar controller
 @Controller
@@ -21,6 +23,28 @@ class IndexController {
 
 		// Agregar mensaje "message" al modelo
 		model.addAttribute("message", "Ivan !");
+
+		return "index";
+	}
+
+	// /message/{name}
+	@RequestMapping(value = "/message/{name}", method = RequestMethod.GET)
+	public String showIndexPage2(Model model,
+			@PathVariable("name") String name) {
+
+		// Agregar mensaje "message" al modelo
+		model.addAttribute("message", name + " !");
+
+		return "index";
+	}
+
+	// /name?name={name}
+	@RequestMapping(value = "/name", method = RequestMethod.GET)
+	public String showIndexPage3(Model model,
+			@RequestParam("name") String name) {
+
+		// Agregar mensaje "message" al modelo
+		model.addAttribute("message", name + " !");
 
 		return "index";
 	}
--- 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica28/controller/admin/AdminController.java
+++ 5-spring-web/5-spring-web-mvc-hello-world/src/main/java/org/certificatic/spring/mvc/practica28/controller/admin/AdminController.java
@@ -4,8 +4,10 @@ import org.certificatic.spring.mvc.practica28.rootbeans.BeanComponent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // Definir controller
 @Controller
@@ -24,6 +26,33 @@ class AdminController {
 
 		// enviar mensaje "message" al modelo
 		String saludo = beanComponent.sayHello("Ivan");
+
+		model.addAttribute("message", saludo);
+
+		return "admin";
+	}
+
+	// /message/{name}
+	@RequestMapping(value = "/message/{name}", method = RequestMethod.GET)
+	public String showIndexPage2(Model model,
+			@PathVariable("name") String name) {
+
+		// Agregar mensaje "message" al modelo
+		// enviar mensaje "message" al modelo
+		String saludo = beanComponent.sayHello(name);
+
+		model.addAttribute("message", saludo);
+
+		return "admin";
+	}
+
+	// /name?name={name}
+	@RequestMapping(value = "/name", method = RequestMethod.GET)
+	public String showIndexPage3(Model model,
+			@RequestParam("name") String name) {
+
+		// Agregar mensaje "message" al modelo
+		String saludo = beanComponent.sayHello(name);
 
 		model.addAttribute("message", saludo);
 
